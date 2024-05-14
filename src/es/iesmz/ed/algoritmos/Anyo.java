ad6669dd4ad103b4f1d098f0511e63b94167bbf8
javi <javii.oteero@gmail.com>
2024-05-14 12:57:45 +0200

    Pasados tests menos caso negativo

--- src/es/iesmz/ed/algoritmos/Anyo.java
+++ src/es/iesmz/ed/algoritmos/Anyo.java
@@ -1,29 +1,42 @@
 package es.iesmz.ed.algoritmos;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * Esta clase crea objetos capaces de identificar el número distinto de dígitos en un número
- * @author Sopacaser
+ * Esta clase crea objetos capaces de identificar el número distinto de dígitos en un año
+ * @author Sopacasera
  * @version 1.0 14/05/2024
  */
 
 public class Anyo {
 
-    //Campo de la clase
+    // Campo de la clase
     private long anyo;
 
     /**
      * Constructor para la clase
-     * @param anyo el anyo a comprobar los digitos distintos
+     * @param anyo el año a comprobar los dígitos distintos
      */
     public Anyo(long anyo){
         this.anyo = anyo;
     }
 
     /**
-     * Metodo para saber cuantos digitos distintos hay
-     * @return el numero de digitos distintos
+     * Método para calcular el número de dígitos diferentes en el año
+     * @return el número de dígitos diferentes
      */
     public int digitosDiferentes(){
-        return 0;
+        String anyoStr = Long.toString(anyo);
+        List<Character> digitosUnicos = new ArrayList<>();
+
+        for (int i = 0; i < anyoStr.length(); i++) {
+            char digito = anyoStr.charAt(i);
+            if (!digitosUnicos.contains(digito)) {
+                digitosUnicos.add(digito);
+            }
+        }
+
+        return digitosUnicos.size();
     }
 }
