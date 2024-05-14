64c2c67c35b338292f7d09dcf93fb2ef048c533c
javi <javii.oteero@gmail.com>
2024-05-14 12:22:42 +0200

    Creada clase hiperpar y tests

---
+++ src/es/iesmz/ed/algoritmos/Hyperpar.java
@@ -0,0 +1,19 @@
+package es.iesmz.ed.algoritmos;
+
+/**
+ * Esta clase define objetos que definen si un número es hiperpar
+ * @author: Sopacasera
+ * @version: 1.0 14/05/2024
+ *
+ */
+public class Hyperpar {
+
+    /**
+     * Método para saber si un número es hiperpar
+     * @param num el número del que se quiere saber si es hiperpar
+     * @return true si es hiperpar, false si no lo es
+     */
+    public static boolean isHyperpar(long num){
+        return true;
+    }
+}
---
+++ src/es/iesmz/ed/algoritmos/HyperparTest.java
@@ -0,0 +1,31 @@
+package es.iesmz.ed.algoritmos;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HyperparTest {
+    @Test
+    void testHiperpar(){
+        assertTrue(Hyperpar.isHyperpar(24680));
+    }
+
+    @Test
+    void testNoHiperpar(){
+        assertFalse(Hyperpar.isHyperpar(2568));
+    }
+
+    @Test
+    void test0(){
+        assertTrue(Hyperpar.isHyperpar(0));
+    }
+
+    @Test
+    void testHiperparNegativo(){
+        assertTrue(Hyperpar.isHyperpar(-2684));
+    }
+
+    @Test
+    void testNoHiperparNegativo(){
+        assertFalse(Hyperpar.isHyperpar(1256));
+    }
+}
